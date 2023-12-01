814e58416287b62f553c483a1cd25ba5ac94f141
@hexlet/cli <support@hexlet.io>
2023-12-01 11:47:13 +0300

    submit java-spring-ru/first-start

--- java-spring-ru/first-start/build.gradle.kts
+++ java-spring-ru/first-start/build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    // BEGIN
-    
-    // END
+    java
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
     application
     id("com.github.ben-manes.versions") version "0.48.0"
 }
@@ -21,7 +21,10 @@ repositories {
 
 dependencies {
     // BEGIN
-    
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     // END
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
